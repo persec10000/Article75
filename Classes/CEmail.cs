df45ae3dc86f860317a97e19d02a7015cfd3dca9
persec10000 <persec10000@eclipso.eu>
2021-01-21 19:06:56 +0300

    Add files via upload

--- Classes/CEmail.cs
+++ Classes/CEmail.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MailKit.Security;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MimeKit.Text;
+using System;
 using System.Net.Mail;
 
 namespace Article75
@@ -25,12 +29,49 @@ namespace Article75
             this.server = server;
             this.password = password;
 
-            this.mittente = "richworld3tai@gmail.com";
-            this.server = "smtp.gmail.com";
+            this.mittente = "notifiche@articolo75.it";
+            this.server = "smtpa1.aruba.it";
+
+
+
         }
 
         public bool Invio()
         {
+            try
+            {
+                // create email message
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse("notifiche@articolo75.it"));
+                email.To.Add(MailboxAddress.Parse(destinatario));
+                email.Subject = oggetto;
+                email.Body = new TextPart(TextFormat.Html) { Text = "<h1>Example HTML Message Body</h1>" };
+                if (password.Length > 0)
+                {
+                    msg += "Password : " + password;
+                }
+
+                if (link.Length > 0)
+                    email.Body = new TextPart(TextFormat.Html) { Text = messaggio + "<a href=\"" + link + "\"" + ">Clicca qui</a>" + msg }; 
+                else
+                    email.Body = new TextPart(TextFormat.Html) { Text = messaggio }; 
+                // send email
+                var smtp = new MailKit.Net.Smtp.SmtpClient();
+                smtp.Connect("smtps.aruba.it", 465, SecureSocketOptions.SslOnConnect); 
+                smtp.Authenticate("notifiche@articolo75.it", "blD5s6YLFT++");
+                smtp.Send(email);
+                smtp.Disconnect(true);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error sending email \n" + ex.ToString());
+                return false;
+            }
+            return true;
+
+            // -----------------
+            /*
             try
             {
                 MailMessage mail = new MailMessage();
@@ -52,18 +93,19 @@ namespace Article75
                 //SmtpClient client = new SmtpClient(server);
                 //client.UseDefaultCredentials = true;
                 //client.Send(mail);
-                SmtpClient client = new SmtpClient( "smtp.gmail.com", 587);//587
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient(this.server, 25);//587
                 //client.Port = 80;
                 //client.EnableSsl = false;
                 ///client.DeliveryMethod = SmtpDeliveryMethod.Network;
                 //client.UseDefaultCredentials = true;
-                System.Net.NetworkCredential basicCredential1 = new System.Net.NetworkCredential(mittente,                                     "t7u9z6msg999");
+                //System.Net.NetworkCredential basicCredential1 = new System.Net.NetworkCredential(mittente, "blD5s6YLFT++");
                 client.EnableSsl = true;
                 client.UseDefaultCredentials = false;
                 client.Credentials = basicCredential1;
                 
                 client.Send(mail);
                 return true;
+
             }
             catch (Exception ex)
             {
@@ -79,6 +121,7 @@ namespace Article75
 
                 return false;
             }
+            */
         }
     }
 }
